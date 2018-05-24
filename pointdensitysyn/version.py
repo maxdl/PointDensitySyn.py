e8d34656911262250b65f4a3fa4843c904968ac3
Max Larsson <max.larsson@liu.se>
2018-05-24 09:18:50 +0200

    Bumped version

--- pointdensitysyn/version.py
+++ pointdensitysyn/version.py
@@ -3,8 +3,8 @@ import sys
 
 title = "PointDensitySyn"
 author = "Max Larsson"
-version = "1.2.0"
-date = ("May", "8", "2018")
+version = "1.3.0"
+date = ("May", "24", "2018")
 email = "max.larsson@liu.se"
 homepage = "www.liu.se/medfak/forskning/larsson-max/software"
 if hasattr(sys, 'frozen'):
